dbc0365aa9d2573e1feb9354f95cb6de04858656
MAHMOUD-ALHAJJOO <mahmudalhajjo.95@gmail.com>
2025-08-28 17:18:40 +0300

    feat: add public key for dev

--- services/api/client.ts
+++ services/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE = "https://app.ticketmaster.com/discovery/v2";
-const KEY = process.env.EXPO_PUBLIC_TM_KEY ?? "";
+const KEY = process.env.EXPO_PUBLIC_TM_KEY ?? "O4GB1HldF10Qw10XCMOuizfq6KubhnhD";
 
 export const api = axios.create({
   baseURL: BASE,
